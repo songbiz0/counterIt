aa10d59057f81c78a74ae37afa5eaecf614309d9
Kim Minjae <songbiz00@gmail.com>
2022-07-31 10:55:01 +0900

    Initial commit

---
+++ src/main/java/com/counterit/CounterItApplication.java
@@ -0,0 +1,13 @@
+package com.counterit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/counterit/CounterItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counterit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
